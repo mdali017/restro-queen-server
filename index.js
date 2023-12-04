d7438496be20e2f3e072f6fd9d90b70577ffea76
MD MOHABBAT ALI <mohabbatalit8@gmail.com>
2023-12-04 10:28:47 +0600

    Admin Home and User Home related api created

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ async function run() {
     // =============== Payment Related Api
     app.post("/create-payment-intent", verifyJWT, async (req, res) => {
       const { price } = req.body;
-      const amount = price * 100;
+      const amount = Math.round(parseFloat(price) * 100);
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
         currency: "usd",
